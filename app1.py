05781aca36451728c45b753a8436bb1280340d4b
Aleksandr Pimenov <167337375+wachawo@users.noreply.github.com>
2024-09-27 22:45:33 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -11,12 +11,12 @@ from watchdog.events import FileSystemEventHandler
 
 PORT = 514
 EVENT = threading.Event()
-LOGPATH = 'logs'
+LOG_PATH = 'logs'
 
 LOGGING = {
     'handlers': [
         logging.StreamHandler(),
-        logging.handlers.RotatingFileHandler(filename=f'{LOGPATH}/app1.log', maxBytes=1024*1024*1024, backupCount=10),
+        logging.handlers.RotatingFileHandler(filename=f'{LOG_PATH}/app1.log', maxBytes=1024*1024*1024, backupCount=10),
     ],
     'format': '%(asctime)s.%(msecs)03d [%(levelname)s]: (%(name)s) %(message)s',
     'level': logging.DEBUG,
@@ -110,9 +110,8 @@ def start_file_watcher():
     return obsrv
 
 if __name__ == "__main__":
-    # Check and create the 'logs' directory if it doesn't exist
-    if not os.path.exists(LOGPATH):
-        os.makedirs(LOGPATH)
+    if not os.path.exists(LOG_PATH):
+        os.makedirs(LOG_PATH)
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
     ob_server = start_file_watcher()
